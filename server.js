cb32bfbf34bb0ed5920beffeb1e1bf69ee38a552
jozais-dej <72046539+jozais-dej@users.noreply.github.com>
2021-05-17 17:02:13 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,60 @@
  * Write your server code in this file.  Don't forget to add your name and
  * @oregonstate.edu email address below, so we know whose code we're grading.
  *
- * name:
- * email:
+ * name: Jozias de Jonge
+ * email: dejongej@oregonstate.edu
  */
+let http = require("http");
+let fs = require('fs');
+
+
+let htmlContent = fs.readFileSync('public/index.html');
+let cssContent = fs.readFileSync('public/style.css');
+let jsContent = fs.readFileSync('public/index.js');
+let fourContent = fs.readFileSync('public/404.html');
+
+console.log("PORT:", process.env.PORT);
+
+function requestHandler(req, res) {
+    console.log("method:", req.method);
+    console.log("url:", req.url);
+    console.log("headers:", req.headers);
+
+    if ((req.url == '/index.html') || (req.url == '/')) {
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+        res.write(htmlContent);
+    }
+    else if (req.url == '/style.css') {
+        res.writeHead(200, {
+            "Content-Type": "text/css"
+        });
+        res.write(cssContent);
+    }
+    else if (req.url == '/index.js') {
+        res.writeHead(200, {
+            "Content-Type": "text/js"
+        });
+        res.write(jsContent);
+    }
+    else if (req.url == '/404.html') {
+        res.writeHead(200, {
+            "Content-Type": "text/html"
+        });
+        res.write(fourContent);
+    }
+    else {
+        res.writeHead(404, {
+            "Content-Type": "text/html"
+        });
+        res.write(fourContent);
+    }
+    res.end();
+}
+
+let server = http.createServer(requestHandler);
+
+server.listen(3000, function (err) {
+    console.log("Server running on port 3000");
+});
